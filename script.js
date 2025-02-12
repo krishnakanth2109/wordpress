7a2a45584b97e345533a42ab06aceb59e3aca197
krishnakanth2109 <146426157+krishnakanth2109@users.noreply.github.com>
2025-02-12 11:54:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Contact Form Submission
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+    document.getElementById('contactForm').reset();
+  });
